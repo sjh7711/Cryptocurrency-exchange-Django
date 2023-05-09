67edb807093331fd9d5f84b3d0f8cd7f5a2963c9
sjh7711 <apfhd159862@naver.com>
2023-05-09 13:38:54 +0900

    login

--- app_main/urls.py
+++ app_main/urls.py
@@ -5,6 +5,7 @@ from app_main import views
 urlpatterns = [
     path('', views.main, name="main"),
     path('try_login', views.try_login, name="try_login"),
+    path('try_logout', views.try_logout, name="try_logout"),
     path('get_value', views.get_value, name='get_value'),
 ]
 
