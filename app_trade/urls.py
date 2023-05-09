c6774628bf466613550384bccf86d01c85652ea4
kormusclepig <bemy.door@gmail.com>
2023-05-10 06:16:05 +0900

    주현철

--- app_trade/urls.py
+++ app_trade/urls.py
@@ -4,6 +4,5 @@ from app_trade import views
 
 urlpatterns = [
     path('', views.trade, name="trade"),
-    path('get_prices/', views.get_prices, name='get_prices'),
-    path('trade_coin/',views.trade_coin, name='trade_coin'),
+    path('submit/', views.submit, name="submit")
 ]
